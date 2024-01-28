1cf395a6978f3699bb4826a809c5cea37e02c1a5
kdeebes <deebes.kerry21@gmail.com>
2024-01-27 20:40:27 -0400

    w02Assignment

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,55 +3,67 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+    let fullName = "Kerryann Deebes";
 
-let fullName = 'Kerryann Deebes';
+//Declare and instantiate a variable to hold the current year.
+    let currentYear = new Date().getFullYear();
 
-var currentYear = new Date().getFullYear();
-var profilePicture = "images/profilePicture.jpg";
+//Declare and instantiate a variable to hold the file path
+// Identify the variable as "profilePicture".
+    let profilePicture = "images/profilePicutre.jpg"
+
+
 
 /* Step 3 - Element Variables */
 
-const nameElement = document.getElementById('name');
-const foodElement = document.getElementById("food");
-const yearElement = document.querySelector("#year");
+    const nameElement = document.getElementById("name");
+    const foodElement = document.getElementById("food");
+    const yearElement = document.querySelector("#year");
 
-// Get the profile image element by its ID
-var imageElement = document.querySelector('home');
+//Use any viable method to get the profile image element stored into a variable named "imageElement".
+    const imageElement = document.querySelector("main picture img");
 
 /* Step 4 - Adding Content */
+//Assign the nameElement's innerHTML property the fullName variable value.
+//Surround the fullName value with <strong> tags using a template literal.
+    nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
-nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.textContent = currentYear;
-imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+//Use the textContent property to set the value of the element to the value of the variable currentYear.
+    yearElement.textContent = currentYear;
+
+//Use the setAttribute method to set the src property of the image element.
+    imageElement.setAttribute("src", profilePicture);
+
+    //Use the setAttribute method to set the alt property of the image element.
+    imageElement.setAttribute("alt", `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 
-/* ARRAYS */
+//Declare an array variable to hold your favorite foods.
+    let favoriteFoods = ["Red Peas Cookup with Beef", "Baked Macaroni & Cheese", "Vegetable Rice with Baked Chicken"];
 
-// Step 1: declare and instantiate an array variable to hold your favorite foods
-let FavFoods = ["Macaroni & Cheese ", " Curry & Roti ", " Red Peas Cookup with Beef ", " Fried Plantains with Eggs & bacon"];
+//Modify the HTML element with the id of food to display your favorite foods array.
+    foodElement.textContent = favoriteFoods.join(", ");
 
-// Step 2: place the values of the favorite foods variable into the HTML file
-foodElement.innerHTML = FavFoods.join('<br>');
+//Declare and instantiate a variable to hold another single favorite food item.
+    let additionalFood = "otherFoods";
 
-// Step 3: declare and instantiate a variable to hold another favorite food
-let FavFoodOther = " Potato & Ripe Plantain Salad";
+//Add the value stored in this new variable to your favorite food array.
+    favoriteFoods.push(otherFoods);
 
-// Step 4: add the variable holding another favorite food to the favorite food array
-FavFoods.push(FavFoodOther);
+//Append the new array values onto the displayed content of the HTML element with the id of food.
+//Use a += operator and a <br> (line break character).
 
-// Step 5: Append the new array values onto the displayed content of the HTML element
-foodElement.innerHTML += `<br>${FavFoods.join('<br>')}`;
+    foodElement.innerHTML += "<br>" + favoriteFoods.join(", ");
 
-// Step 6: remove the first element in the favorite foods array
-FavFoods.shift();
+//Remove the first element in the favorite food array.
+    favoriteFoods.shift();
 
-// Step 7: repeat Step 5
-foodElement.innerHTML = FavFoods.join('<br>');
+//Again, append the array output showing the modified array, using a line break.
+    foodElement.innerHTML += "<br>" + favoriteFoods.join(", ");
 
-// Step 8: remove the last element in the favorite foods array
-FavFoods.pop();
+//Remove the last element in the favorite food array.
+    favoriteFoods.pop();
 
-// Step 9: repeat Step 2
-foodElement.innerHTML = FavFoods.join('<br>');
+//Append the array output with this final modified favorite foods array.
+    foodElement.innerHTML += "<br>" + favoriteFoods.join(", ");
